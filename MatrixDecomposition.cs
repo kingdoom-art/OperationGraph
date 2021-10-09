eefeca3a43454315172c0cdf546c381788116c4e
Бартов Евгений Игоревич <bartov-2016@mail.ru>
2021-10-09 20:03:57 +0500

    master: доделал последнее задание, закомментил код, добавил ООП

---
+++ MatrixDecomposition.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class MatrixDecomposition
+    {
+        //достижимое
+        List<int> R = new List<int>();
+        //контрдостижимое
+        List<int> Q = new List<int>();
+        //цикл графа
+        List<int> resultPlenty = new List<int>();
+        int iter;
+        public string name;
+
+        public MatrixDecomposition(int iter)
+        {
+            this.iter = iter;
+            name = "V" + iter;
+        }
+
+        /// <summary>
+        /// Проверяет наличие вершины в множестве достижимости
+        /// </summary>
+        /// <param name="tmp">Номер вершины</param>
+        public bool R_Contains(int tmp)
+        {
+            return R.Contains(tmp);
+        }
+
+        /// <summary>
+        /// Проверяет наличие вершины в множестве контрдостижимости
+        /// </summary>
+        /// <param name="tmp">Номер вершины</param>
+        public bool Q_Contains(int tmp)
+        {
+            return Q.Contains(tmp);
+        }
+
+        /// <summary>
+        /// ДОбавление вершины в множество достежимости, если такая вершина есть, не добавится
+        /// </summary>
+        /// <param name="tmp">Номер добавляемой вершины</param>
+        public void Add_R(int tmp)
+        {
+            //если вершины еще нет, добавляем
+            if (!R.Contains(tmp)) R.Add(tmp);
+        }
+
+        /// <summary>
+        /// Добавление вершины в множество контрдостижимости, если такая вершина есть, не добавится
+        /// </summary>
+        /// <param name="tmp">Номер добавляемой вершины</param>
+        public void Add_Q(int tmp)
+        {
+            //если вершины еще нет, добавляем
+            if (!Q.Contains(tmp)) Q.Add(tmp);
+        }
+
+        /// <summary>
+        /// Вывод множества достижимости
+        /// </summary>
+        public void ShowR()
+        {
+            Console.WriteLine("Достижимое множество Итеррация:" + iter);
+            R.Sort();
+            R.ForEach(t => Console.Write(t + " "));
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Вывод множества контрдостижимости
+        /// </summary>
+        public void ShowQ()
+        {
+            Console.WriteLine("Контрдостижимое множество Итеррация:" + iter);
+            Q.Sort();
+            Q.ForEach(t => Console.Write(t + " "));
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Вывод пересечения множеств достижимости и контрдостижимости
+        /// </summary>
+        public void ShowIntersect()
+        {
+            Console.WriteLine("Пересечени множеств Итеррация:" + iter);
+            resultPlenty.Sort();
+            resultPlenty.ForEach(t => Console.Write(t + " "));
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Операция пересечения множеств
+        /// </summary>
+        public void Intersect()
+        {
+            //Пересечение множеств
+            resultPlenty = R.Intersect(Q).ToList();
+        }
+
+        /// <summary>
+        /// Удаление вершин цикла из исходной последовательности вершин
+        /// </summary>
+        /// <param name="point">Последовательность вершин</param>
+        public List<int> ShrederPoint(List<int> point)
+        {
+            //разности множеств всех вершин и вершин цикла
+            var result = point.Except(resultPlenty).ToList();
+            return result;
+        }
+
+        /// <summary>
+        /// Удаление связей вершин цикла с остальными в графе
+        /// </summary>
+        /// <param name="mas">Исходный граф</param>
+        public List<Edge> ShrederEdge(List<Edge> mas)
+        {
+            //Идем по всем вершинам, если находим исходящую или входящую, которая была в цикле, пропускаем
+            var result = mas.Where(r => !resultPlenty.Contains(r.a) & !resultPlenty.Contains(r.b)).ToList();
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,21 +8,24 @@ using System.Resources;
 
 namespace Graph
 {
+    struct Edge
+    {
+        public int a, b;
+        public int num;
+
+        public Edge(string a, string b, string num)
+        {
+            this.a = Convert.ToInt32(a);
+            this.b = Convert.ToInt32(b);
+            this.num = Convert.ToInt32(num);
+        }
+    }
+
     class Program
     {
-        struct Edge
-        {
-            public int a, b;
-            public int num;
+        
 
-            public Edge(string a, string b, string num)
-            {
-                this.a = Convert.ToInt32(a);
-                this.b = Convert.ToInt32(b);
-                this.num = Convert.ToInt32(num);
-            }
-        }
-
+        //получение графа формата (Первая_вершина Вторя_вершина Номер_пути)
         static List<Edge> GetGraph()
         {
             List<Edge> mas = new List<Edge>();
@@ -45,55 +48,69 @@ namespace Graph
             return mas;
         }
 
+        //Вывод матицы смежности
         static void ShowMatrix(List<Edge> mas, List<int> point)
         {
             Console.Write("   ");
             point.ForEach(x => Console.Write(String.Format("{0,3}",x)));
             Console.WriteLine();
 
-
+            //идем по отсортированным, зарание вершинам
             foreach (var i in point)
             {
                 Console.Write(String.Format("{0,3}",i));
                 int[] result = new int[point.Count()];
+                //находим вершину. в которую есть выход, заполняем 1
                 mas.Where(r => r.a == i).ToList().ForEach(t => result[t.b-1] = 1);
+                //выводим получившийся массив
                 result.ToList().ForEach(m => Console.Write(String.Format("{0,3}",m)));
                 Console.WriteLine();
             }
 
         }
 
+        //Матрица инцеденций
         static void ShowIncidents(List<Edge> mas, List<int> point)
         {
+            //сортируем по номерам пути
             var reb = (from i in mas orderby i.num select i.num).ToList();
             Console.Write("   ");
             reb.ForEach(t => Console.Write(String.Format("{0,3}",t)));
             Console.WriteLine();
 
+            //идем по отсортированным путям, чтобы не создавать временных массивов
             foreach (var i in point)
             {
                 Console.Write(String.Format("{0,3}",i));
                 int[] result = new int[reb.Count()];
+                //заполняем выходной массив по достижимости вершин путем
                 mas.Where(t => t.a == i).ToList().ForEach(r => result[r.num - 1] = 1);
                 mas.Where(t => t.b == i).ToList().ForEach(r => result[r.num - 1] = -1);
+                //выводим получившийся массив
                 result.ToList().ForEach(m => Console.Write(String.Format("{0,3}", m)));
                 Console.WriteLine();
 
             }
         }
 
+        //множественное задание структуры
         static void ShowPlenty(List<Edge> mas)
         {
+            //немного sql групперуем первой вершине, навыходе получим страшную структуру данных
             var tmp = from i in mas group i by i.a;
 
             foreach(var i in tmp)
             {
+                //ключ это вершина, из которой будем извлекать достижимые вершины
                 Console.Write("G("+i.Key+")=(");
                 string res = "";
+                //извлекаем группу и записываем сразу в строку
                 i.ToList().ForEach(t => res += "," + t.b);
+                //ну и удаляем первый символ, ибо он запятая
                 Console.WriteLine(res.Remove(0,1) + "); ");
             }
 
+            //теперь групперуем по второй вершине
             var tmp2 = from i in mas group i by i.b;
 
             foreach (var i in tmp2)
@@ -105,44 +122,75 @@ namespace Graph
             }
         }
 
+        //Декомпозиция
         static void Decomposition(List<Edge> mas, List<int> point)
         {
-            Stack<int> tmpStack = new Stack<int>();
-            List<int> R = new List<int>();
-            List<int> Q = new List<int>();
-
-            //находим достижимое
-            tmpStack.Push(point[0]);
-
-            while (tmpStack.Count() > 0)
+            int iter = 1;
+            List<MatrixDecomposition> finalGraph = new List<MatrixDecomposition>();
+            while(point.Count > 0)
             {
-                int tmp = tmpStack.Pop();
-                if (!R.Contains(tmp)) R.Add(tmp);
+                MatrixDecomposition tmpGraph = new MatrixDecomposition(iter);
+                Stack<int> tmpStack = new Stack<int>();
 
-                var plenty = mas.Where(r => r.a == tmp && !R.Contains(r.b)).Select(m => m.b).ToList();
-                plenty.ForEach(t => tmpStack.Push(t));
+                //запихиваем вершину в стэк
+                tmpStack.Push(point[0]);
+
+                while (tmpStack.Count() > 0)
+                {
+                    //достаем верхнюю вершину стэка, в стэке она удаляется
+                    int tmp = tmpStack.Pop();
+                    tmpGraph.Add_R(tmp);
+                    //Console.WriteLine(iter + " R");
+                    /*
+                     * идем по графу, ищем нашу вершину на первом местие, при этом вершина на втором месте
+                     * не должна содержаться в финальном множестве
+                     * достаем вторю вершину, по скольку в нее будет идти путь из первой
+                    */
+                    var plenty = mas.Where(r => r.a == tmp && !tmpGraph.R_Contains(r.b)).Select(m => m.b).ToList();
+                    //запишем весь набор получившихся вершин в стэк
+                    plenty.ForEach(t => tmpStack.Push(t));
+                }
+
+                //контрдостижимое
+                tmpStack.Push(point[0]);
+                while (tmpStack.Count() > 0)
+                {
+                    int tmp = tmpStack.Pop();
+                    tmpGraph.Add_Q(tmp);
+                    //Console.WriteLine(iter + " Q");
+                    //Аналогично достижимому, только проверяется на первая вершина, а вторая
+                    var plenty = mas.Where(r => r.b == tmp && !tmpGraph.Q_Contains(r.a)).Select(m => m.a).ToList();
+                    plenty.ForEach(t => tmpStack.Push(t));
+                }
+                //Делаем песечение множеств
+                tmpGraph.Intersect();
+                //удаляем все отработанное из графа
+                point = tmpGraph.ShrederPoint(point);
+
+                point.ForEach(t => Console.Write(t + " "));
+                Console.WriteLine();
+
+                mas = tmpGraph.ShrederEdge(mas);
+                mas.ForEach(t => Console.Write(t.a + " " + t.b + " " + t.num+" | "));
+                Console.WriteLine();
+
+                Console.WriteLine(tmpGraph.name);
+                tmpGraph.ShowR();
+                tmpGraph.ShowQ();
+                tmpGraph.ShowIntersect();
+
+                //собираем новый граф
+                finalGraph.Add(tmpGraph);
+                iter++;
             }
 
-            R.ForEach(t => Console.Write(t + " "));
-            Console.WriteLine();
-
-            //контрдостижимое
-            tmpStack.Push(point[0]);
-            while(tmpStack.Count() > 0)
-            {
-                int tmp = tmpStack.Pop();
-                if (!Q.Contains(tmp)) Q.Add(tmp);
-
-                var plenty = mas.Where(r => r.b == tmp && !Q.Contains(r.a)).Select(m => m.a).ToList();
-                plenty.ForEach(t => tmpStack.Push(t));
-            }
-            Q.ForEach(t => Console.Write(t + " "));
-            Console.WriteLine();
-
-            //Пересечение множеств
-            var resultPlenty = R.Intersect(Q).ToList();
-
-            resultPlenty.ForEach(t => Console.Write(t + " "));
+            //foreach(var i in finalGraph)
+            //{
+            //    Console.WriteLine(i.name);
+            //    i.ShowR();
+            //    i.ShowQ();
+            //    i.ShowIntersect();
+            //}
         }
 
         static void Main(string[] args)
